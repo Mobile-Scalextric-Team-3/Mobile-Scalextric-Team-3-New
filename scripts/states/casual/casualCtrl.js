763d485449e81867f6d4686a3eba8e85a4c585d3
JamesSweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-02-26 16:32:51 +0000

    Update casualCtrl.js

--- scripts/states/casual/casualCtrl.js
+++ scripts/states/casual/casualCtrl.js
@@ -16,7 +16,7 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
 
     var channel = $stateParams.channel;//sets channel to one sent from previous state
 
-    //action box texzt setter
+    //function controls action box
     function actionUsed(resourceId){
         var target = vm.targetChannel;
         var div = angular.element(document.querySelector('#action'));
@@ -31,63 +31,61 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
         }
         
         if(resourceId == 1){
-            div.html('Oil slick used on ' + target + " car");
+            div.html('Oil slick used on ' + target + ' car');
         }
         else if(resourceId == 2){
-            div.html('Puncture used on ' + target + " car");
+            div.html('Puncture used on ' + target + ' car');
         }
         else if(resourceId == 3){
-            div.html('Smart bomb used on ' + target + " car");
+            div.html('Smart bomb used on ' + target + ' car');
         }        
     }
 
     vm.actionUsed = actionUsed;
 
-    //Car control starts here
     const DEFAULT_THROTTLE = 0;
 
     vm.throttle = DEFAULT_THROTTLE;
     vm.actualThrottle = DEFAULT_THROTTLE;
     vm.resources = [];
 
+    /*
     //sets target channels
     vm.targetChannels = Array.apply(null, {
         length: 2
     }).map(Function.call, Number);
-
-    /*
+    
     //sets list to show all but the channel you are on
     vm.targetChannels = vm.targetChannels.filter(targetChannel => targetChannel !== channel );
     console.log(vm.targetChannels);
 
-    //default target channel 0
+    //default target channel -1
     vm.targetChannel = -1;
     */
 
+    //sets target channel as the other channel in race
     if(channel == 0){
         vm.targetChannel = 1;
     }
     else if(channel == 1){
         vm.targetChannel = 0;
-    }
-    
+    }    
 
     vm.throttleError = false;
 
     vm.stop = stop;
     vm.fireSpecialWeapon = fireSpecialWeapon;
 
-
-
-
+    //sets topic details
     var throttleTopic = `${brokerDetails.UUID}/control/${channel}/throttle`;
     var getResourcesTopic = `${brokerDetails.UUID}/resources`;
     var resourceStateTopic = `${brokerDetails.UUID}/control/{channel}/{resourceId}/state`;
 
+    //subscribes to topics
     mqttService.subscribe(throttleTopic);
-
     mqttService.subscribe(getResourcesTopic);
 
+    //when the 'X' button is pressed
     function stop() {
         var payload = {
             set : 0
@@ -98,6 +96,7 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
         $state.transitionTo('onboarding', {});
     }
 
+    //triggered when weapon button clicked
     function fireSpecialWeapon(resourceId) {
         actionUsed(resourceId);
         let payload = {
@@ -107,6 +106,7 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
         mqttService.publish(resourceStateTopic.replace(/\{resourceId\}/, resourceId).replace(/\{channel\}/, channel), JSON.stringify(payload));
     }
 
+    //stops when page is closed or changed
     window.onhashchange = function () {
         if (changed) {
             console.log('changed');
@@ -116,6 +116,8 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
         }
     }
 
+
+    
     mqttService.onMessageArrived(function (message) {
 
         console.log(message);
@@ -143,11 +145,13 @@ function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
         }
 
     });
+    
 
+    //watches for throttle change
     $scope.$watch("casual.throttle", function (newThrottle, oldThrottle) {
         if (newThrottle != oldThrottle) {
             var payload = {
-                set : newThrottle
+                "set": newThrottle
             }
             mqttService.publish(throttleTopic, JSON.stringify(payload));
         }
