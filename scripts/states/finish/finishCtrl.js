91d78ebcaed17f019895d1ab62ba7520d5d717ca
James Sweetland <james.sweetland@live.co.uk>
2020-03-17 19:31:51 +0000

    Added winning and losing

--- scripts/states/finish/finishCtrl.js
+++ scripts/states/finish/finishCtrl.js
@@ -15,28 +15,61 @@ function finishCtrl($scope, $state, $stateParams, stopClock, mqttService, broker
     var channel = $stateParams.channel;
     var bestTime = $stateParams.bestLap;
     var time = $stateParams.time;
+    var theirTime;
 
     stopClock.endClock();
-
-    time = stopClock.timeFormat(time);
-    bestTime = stopClock.timeFormat(bestTime);
 
     var channelName;
 
     if(channel == 0){
-        channelName = "red"
+        channelName = "red";
     }
     else if(channel == 1){
-        channelName = "yellow"
+        channelName = "yellow";
     }
 
     var gameStateTopic = `${brokerDetails.UUID}/control/game_state`;
     mqttService.subscribe(gameStateTopic);
 
-    var div = angular.element(document.querySelector("#finish-message"));
-    div.html('Well done ' + channelName + '<br>Your overall time was: ' + time +
-    '<br>Your best lap was: ' + bestTime);
-    
+    function findWinner(){
+        var payload = {
+            "time": time,
+            "channel": channel            
+        }
+        mqttService.publish(gameStateTopic, JSON.stringify(payload));
+    }
+    findWinner();
+
+    function showScore(){
+        var div = angular.element(document.querySelector("#finish-message"));
+        if(time < theirTime){
+            div.html('Well done ' + channelName + '<br>You Won!<br>Your overall time was: ' + stopClock.timeFormat(time) +
+        '<br>Your best lap was: ' + stopClock.timeFormat(bestTime));
+        }
+        else if(theirTime < time){
+            div.html('Well done ' + channelName + '<br>You Lost!<br>Your overall time was: ' + stopClock.timeFormat(time) +
+        '<br>Your best lap was: ' + stopClock.timeFormat(bestTime));
+        }
+        else{
+            div.html('Well done ' + channelName + '<br>Its a Draw!<br>Your overall time was: ' + stopClock.timeFormat(time) +
+        '<br>Your best lap was: ' + stopClock.timeFormat(bestTime));
+        }
+        
+    }
+
+    mqttService.onMessageArrived(function (message) {
+        console.log(message);
+
+        if(message.topic === gameStateTopic){
+            var gameState = JSON.parse(message.payloadString);
+            if(gameState.hasOwnProperty("time")){
+                if(gameState.channel != channel){
+                    theirTime = gameState.time;
+                    showScore();
+                }
+            }
+        }
+    });
 
     function exit(){
         mqttService.disconnect();
